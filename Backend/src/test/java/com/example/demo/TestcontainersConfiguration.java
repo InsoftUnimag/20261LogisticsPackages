249ae3295a5ca265508de1ff2eeccbfc99e6a7e6
Carlos Sehuanes <Carlossehuanes1029@gmail.com>
2026-04-19 23:34:53 -0500

    Chore(Tests): Configurar Testcontainers para PostgreSQL y LocalStack >Implementar infraestructura de pruebas con contenedores para automatizar el ciclo de vida de la base de datos y los servicios de AWS (SQS y S3)de forma aislada.

--- Backend/src/test/java/com/example/demo/TestcontainersConfiguration.java
+++ Backend/src/test/java/com/example/demo/TestcontainersConfiguration.java
@@ -3,30 +3,38 @@ package com.example.demo;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.kafka.KafkaContainer;
-import org.testcontainers.postgresql.PostgreSQLContainer;
-import org.testcontainers.rabbitmq.RabbitMQContainer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:10.10.0"));
+    }
 
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
-	}
+    // 1. Instancia estática (se crea una sola vez)
+    static final LocalStackContainer localstack = new LocalStackContainer(
+            DockerImageName.parse("localstack/localstack:3.4.0")
+    ).withServices(LocalStackContainer.Service.SQS, LocalStackContainer.Service.S3);
 
-	@Bean
-	@ServiceConnection
-	RabbitMQContainer rabbitContainer() {
-		return new RabbitMQContainer(DockerImageName.parse("rabbitmq:latest"));
-	}
+    @Bean
+    LocalStackContainer localstackContainer() {
+        // 2. El bean solo expone la instancia estática
+        return localstack;
+    }
 
+    @DynamicPropertySource
+    static void initializeLocalStackProperties(DynamicPropertyRegistry registry) {
+        // 3. Usamos la instancia estática directamente
+        registry.add("spring.cloud.aws.endpoint", localstack::getEndpoint);
+        registry.add("spring.cloud.aws.credentials.access-key", localstack::getAccessKey);
+        registry.add("spring.cloud.aws.credentials.secret-key", localstack::getSecretKey);
+        registry.add("spring.cloud.aws.region.static", localstack::getRegion);
+    }
 }
